f3ba6a51662676b11763bd672cb854896c1a7a6d
Ida Christine Krokeide <ida.krokeide@sparebank1.no>
2023-04-13 09:52:09 +0200

    complete day 4

--- 04/rust4/src/main.rs
+++ 04/rust4/src/main.rs
@@ -5,31 +5,45 @@ fn main() {
     let file_path = "src/input.txt";
     let content = read_file(file_path);
     let values = content.lines();
+    let mut count_full = 0;
+    let mut count_any = 0;
 
     for value in values {
         println!("{}", value);
-
         let parts: Vec<&str> = value.split(',').collect();
-        println!("first: {}, last: {}", parts[0], parts[1]);
 
-        let test:HashSet<i32> =  fill_hash(parts[0]);
-        println!("{:?}", test);
+        let test: HashSet<i32> = fill_hash(parts[0]);
+        let test_2: HashSet<i32> = fill_hash(parts[1]);
+
+        let subset_1 = test.is_subset(&test_2);
+        let subset_2 = test_2.is_subset(&test);
+
+        if subset_1 || subset_2 {
+            count_full += 1;
+        }
+
+        for value in test {
+            if test_2.contains(&value) {
+                count_any += 1;
+                break;
+            }
+        }
+
     }
+
+    println!("Found {:?} fully overlapping pairs", count_full);
+    println!("Found {:?} overlapping pairs", count_any);
 }
 
 fn fill_hash(input: &str) -> HashSet<i32> {
     let mut data = HashSet::new();
     let parts: Vec<&str> = input.split('-').collect();
- let start = parts[0].parse::<i32>().unwrap();
+    let start = parts[0].parse::<i32>().unwrap();
+    let end = parts[1].parse::<i32>().unwrap();
 
- let end = parts[1].parse::<i32>().unwrap();
+    for i in start..end + 1 {
+        data.insert(i);
+    }
 
- println!("start: {}, end {}", start, end);
-
- for i in start..end+1{
-    data.insert(i);
- }
-
- return data;
-    
+    return data;
 }
