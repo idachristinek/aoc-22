6332c9f97cc9c6946176af3cf22a0813cacb13e9
Ida Christine Krokeide <ida.krokeide@sparebank1.no>
2023-04-20 13:58:02 +0200

    continue day 5

--- 05/rust5/src/main.rs
+++ 05/rust5/src/main.rs
@@ -1,15 +1,13 @@
 use rust5::file::read_file;
 
 fn main() {
-    let file_path = "src/input.txt";
+    let file_path = "src/input-test.txt";
     let content = read_file(file_path);
     let values = content.lines();
     let mut cargo: Vec<&str> = Vec::new();
     let mut instructions: Vec<&str> = Vec::new();
 
     for value in values {
-        println!("{}- len: {}", value, value.len());
-
         if value.is_empty() {
         } else if value.chars().nth(0) == Some('m') {
             instructions.push(value);
@@ -18,45 +16,56 @@ fn main() {
         }
     }
 
-    //let mut main_cargo: Vec<Vec<&str>> = Vec::new();
-
-    //check last number in last line in carg to see how many we have
+    // check last number in last line in carg to see how many we have
     let x = cargo.last().unwrap();
-    let mut y: Vec<char> = x.chars().collect();
-    let num = y.get(y.len()-2).unwrap();
-    let size =  num.to_digit(10).unwrap();
+    let y: Vec<char> = x.chars().collect();
+    let num = y.get(y.len() - 2).unwrap();
+    let size = num.to_digit(10).unwrap();
 
-    println!("last line? {:?}", x);
-    println!("number? {:?}", size);
+    let mut main_cargo: Vec<Vec<char>> = Vec::with_capacity(size.try_into().unwrap());
+    for _ in 0..size {
+        let mut col = Vec::with_capacity(cargo.len());
+        for _ in 0..cargo.len() {
+            col.push(' ');
+        }
+        main_cargo.push(col);
+    }
+
+    let mut j = 0;
 
     println!("Cargo:");
- for value in cargo {
-
-        println!("{:?}", value);
+    while j < cargo.len() {
+        println!("{:?}", cargo.get(j));
 
         let mut i = 0;
+        let mut k = 0;
         let mut last_index_added = 0;
         let buffer = 4;
-        let mut letters: Vec<char> = value.chars().collect();
+        let letters: Vec<char> = cargo.get(j).expect("REASON").chars().collect();
 
         while i < letters.len() {
-            println!("{:?}", letters.get(i).unwrap());
+            let working_part: char = *letters.get(i).unwrap();
 
-          if i == 1 || i == letters.len()-2 {
-                println!(" adding first or last {:?} to list", letters.get(i).unwrap());
+            if i == 1 {
                 last_index_added = i;
-            }
-
-             else if i > 2 && i < letters.len()-2 && i == last_index_added + buffer{
-                println!(" i is now {:?}", i);
-                println!(" would add {:?} to list", letters.get(i).unwrap());
+                main_cargo[0][j] = working_part;
+                k += 1;
+            } else if i > 2 && i < letters.len() - 2 && i == last_index_added + buffer {
                 last_index_added = i;
+                main_cargo[k][j] = working_part;
+                k += 1;
+            } else if i == letters.len() - 2 {
+                main_cargo[k][j] = working_part;
+                last_index_added = i;
+                k = 0;
             }
 
             i = i + 1;
         }
+        j += 1;
     }
 
+    println!("{:?}", main_cargo);
     println!("Instructions:");
     for value in instructions {
         println!("{:?}", value);
